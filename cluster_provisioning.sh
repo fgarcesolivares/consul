d571ad750cbf3077624d4ae811a8f43c32f52b7f
Fredys Garces <fgarcesolivares@gmail.com>
2022-10-21 19:55:54 -0500

    Add files via upload

---
+++ cluster_provisioning.sh
@@ -0,0 +1,18 @@
+lines (13 sloc)  907 Bytes
+
+#Install Consul
+wget -O- --no-check-certificate https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+apt install -y consul
+
+#Install Haproxy
+apt install -y haproxy
+systemctl enable haproxy
+
+#Config Haproxy
+curl https://raw.githubusercontent.com/juan-pinzon/compunube/main/consul/resources/haproxy.cfg --output /etc/haproxy/haproxy.cfg
+curl https://raw.githubusercontent.com/juan-pinzon/compunube/main/consul/resources/503sorry.http --output /etc/haproxy/errors/503sorry.http
+service haproxy reload
+
+#Create cluster
+consul agent -ui -server -bootstrap-expect=1 -node=agent-server -bind=192.168.100.2 -client=0.0.0.0 -data-dir=/tmp/consul -config-dir=/etc/consul.d
